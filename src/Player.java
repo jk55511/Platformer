c8c0bedc5cdf8364df30c957dd1cfd43838471dc
6J0ker9 <6J0ker9@DESKTOP-FMJU9SI>
2018-05-31 20:34:32 -0400

    Setup input streams, and packaged spites

--- src/Player.java
+++ src/Player.java
@@ -8,8 +8,12 @@ public class Player extends RealGObject {
     
     public Player(Game game, int y, int x){
         super( game, y, x, 10, 10);
-        try {mySprite = ImageIO.read(new File(myGame.filePath+"\\Sprites\\playerSprite.png"));}
-        catch (IOException a) { System.out.println("No Sprite Found for"+this); System.exit(0);};
+        try {
+        	InputStream image = getClass().getResourceAsStream("/playerSprite.png");
+        	mySprite = ImageIO.read(image); 
+        	image.close();
+        }
+        catch (IOException a) { System.out.println("No Sprite Found for"+this); System.exit(0);}
     }
     
     public void update(){
